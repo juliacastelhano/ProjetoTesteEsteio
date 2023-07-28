9044614dc3fc55370bca7f4902dd0045d04415c7
Julia Castelhano <juliacastelhano@hotmail.com>
2023-07-28 19:33:34 -0300

    Adicionar arquivos de projeto.

---
+++ Ola Mundo/Ola Mundo/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ola_Mundo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BTNText_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Pages.Page1());
+            DisplayAlert("Bem-Vindo", "Seja bem-vindo, " + TXTName.Text + "!", "OK");
+        }
+    }
+}
